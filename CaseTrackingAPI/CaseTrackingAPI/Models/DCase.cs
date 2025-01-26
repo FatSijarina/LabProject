3ec96234c97306a47cdc5d98f35919ae8dba8baf
FatSijarina <fs51701@ubt-uni.net>
2025-01-26 14:37:31 +0100

    Added Case model

---
+++ CaseTrackingAPI/CaseTrackingAPI/Models/DCase.cs
@@ -0,0 +1,14 @@
+﻿namespace CaseTrackingAPI.Models
+{
+    public class DCase
+    {
+        public int Id { get; set; }
+        public string? ImageUrl { get; set; }
+        public string Identifier { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Status { get; set; } = "E Hapur";
+        public string? Details { get; set; }
+        public DateTime DateOpened { get; set; }
+        public DateTime? DateClosed { get; set; }
+    }
+}
