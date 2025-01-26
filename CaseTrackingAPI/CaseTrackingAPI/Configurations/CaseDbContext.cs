0ee5429ef5b9c065f952f09fe472b1a4e6b413a1
FatSijarina <fs51701@ubt-uni.net>
2025-01-26 14:25:38 +0100

    initial commit

---
+++ CaseTrackingAPI/CaseTrackingAPI/Configurations/CaseDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CaseTrackingAPI.Configurations
+{
+    public class CaseDbContext : DbContext
+    {
+        public CaseDbContext(DbContextOptions<CaseDbContext> options) : base(options) 
+        {
+
+        }
+    }
+}
