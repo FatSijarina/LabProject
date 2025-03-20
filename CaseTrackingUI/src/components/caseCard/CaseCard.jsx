2efa574061556c7842b879f1834f6f90a404c0c8
yllisaku <yi51778@ubt-uni.net>
2025-03-20 12:30:55 +0100

    CaseCard
    CaseCard kodi edhe stilizimi

---
+++ CaseTrackingUI/src/components/caseCard/CaseCard.jsx
@@ -0,0 +1,20 @@
+import "./caseCard.scss";
+import { Link } from "react-router-dom";
+const CaseCard = ({ caseId, imgUrl, identifier, title, details, status }) => {
+  return (
+    <div className="card">
+      <img src={imgUrl} alt="Case Card" />
+      <div className="card-text">
+        <p className="card-id">ID: {identifier}</p>
+        <h2 className="card-title">{title}</h2>
+        <p className="card-details">{details}</p>
+      </div>
+      <div className="card-buttons">
+        <Link to={`../case/${caseId}`}>Details</Link>
+        <button className="card-status">{status}</button>
+      </div>
+    </div>
+  );
+};
+
+export default CaseCard;
