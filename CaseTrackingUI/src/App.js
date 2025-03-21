6a3eb8fea29d05fd9fc70f1eea20ec1e97ed21fc
Erosi1 <em52473@ubt-uni.net>
2025-03-21 05:30:02 +0100

    added deshmitari & form components

--- CaseTrackingUI/src/App.js
+++ CaseTrackingUI/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//import Navbar from "./components/navbar/Navbar";
 import TaskList from "./components/TaskCard/TaskCard"; 
 
 const routes = [
